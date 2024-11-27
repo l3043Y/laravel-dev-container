b02d286c70b1e34302a8baff4c2be63e47e8affd
l3043Y <boreyc2013@gmail.com>
2024-11-28 02:20:43 +0700

    add route

--- docker/templates/laravel/routes/api.php
+++ docker/templates/laravel/routes/api.php
@@ -8,3 +8,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::prefix('smartnas/subscriber/v1.0')->group(base_path('routes/subscriber/v1.0.php'));
+Route::get('smartnas/subscriber/up', function(){
+    return ApiResponse::create([
+        'code' => 200,
+        'message' => 'service is up'
+    ]);
+})->name('service-check');
